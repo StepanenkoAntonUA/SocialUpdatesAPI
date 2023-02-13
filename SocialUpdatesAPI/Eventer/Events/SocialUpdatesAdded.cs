1fdb9fb4c61271249bc7b0497e7666bb4c58fb7c
Stepanenko Anton <StepanenkoAntonUa@gmail.com>
2023-02-13 17:35:00 +0200

    edit event methods

--- SocialUpdatesAPI/Eventer/Events/SocialUpdatesAdded.cs
+++ SocialUpdatesAPI/Eventer/Events/SocialUpdatesAdded.cs
@@ -1,26 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SocialUpdatesAPI.Controllers;
-using SocialUpdatesAPI.Models;
 
 namespace Eventer.Events
 {
     public class SocialUpdatesAdded : IEventAction
     {
-        UpdatesController _updatesController;
-        // экземпляр контроллера вообще внутри левой библиотеки????? Это суровое наружение правила отношений между слоями
-        public SocialUpdatesAdded(UpdatesController updatesController)
-        {
-            _updatesController = updatesController;
-        }
-
+        private IEventBus _eventBus;
         public string EventId { get; set; }
         public DateTime EventTime { get; set; }
 
-        public async Task<ActionResult<SocialUpdateDTO>> AddSocialUpdatesItem(SocialUpdate socialUpdate)
-        {
-            // почему внутри IEvent какая-то логика вообще? Это должен быть просто контейнер для данных события
-            var addedItem = await _updatesController.CreateUpdate(socialUpdate);
-            return addedItem;
-        }
     }
 }
--- SocialUpdatesAPI/Eventer/Events/SocialUpdatesDeleted.cs
+++ SocialUpdatesAPI/Eventer/Events/SocialUpdatesDeleted.cs
@@ -11,20 +11,7 @@ namespace Eventer.Events
 {
     public class SocialUpdatesDeleted : IEventAction
     {
-        UpdatesController _updatesController;
-
-        public SocialUpdatesDeleted(UpdatesController updatesController)
-        {
-            _updatesController = updatesController;
-        }
-
         public string EventId { get; set; }
         public DateTime EventTime { get; set; }
-
-        public async Task<ActionResult<SocialUpdateDTO>> DeleteSocialUpdatesItem(Guid id)
-        {
-            var deletedItem = await _updatesController.DeleteUpdate(id);
-            return deletedItem;
-        }
     }
 }

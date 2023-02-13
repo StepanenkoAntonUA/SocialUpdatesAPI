d9a6c8de086de5245a1612968e70df443c454fcd
ssa <ssa.login@gmail.com>
2023-02-13 12:18:42 +0200

    errors

--- SocialUpdatesAPI/Eventer/Events/SocialUpdatesAdded.cs
+++ SocialUpdatesAPI/Eventer/Events/SocialUpdatesAdded.cs
@@ -7,6 +7,7 @@ namespace Eventer.Events
     public class SocialUpdatesAdded : IEventAction
     {
         UpdatesController _updatesController;
+        // экземпляр контроллера вообще внутри левой библиотеки????? Это суровое наружение правила отношений между слоями
         public SocialUpdatesAdded(UpdatesController updatesController)
         {
             _updatesController = updatesController;
@@ -17,6 +18,7 @@ namespace Eventer.Events
 
         public async Task<ActionResult<SocialUpdateDTO>> AddSocialUpdatesItem(SocialUpdate socialUpdate)
         {
+            // почему внутри IEvent какая-то логика вообще? Это должен быть просто контейнер для данных события
             var addedItem = await _updatesController.CreateUpdate(socialUpdate);
             return addedItem;
         }

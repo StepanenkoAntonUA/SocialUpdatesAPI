573e74ba8dad707d516554eddb705bee15a6f76e
Stepanenko Anton <StepanenkoAntonUa@gmail.com>
2023-02-23 17:00:00 +0200

    refactor

--- SocialUpdatesAPI/Eventer/Events/MemoEventBus.cs
+++ SocialUpdatesAPI/Eventer/Events/MemoEventBus.cs
@@ -8,13 +8,7 @@ namespace Eventer.Events
         private ConcurrentDictionary<string, List<IIntegrationEventHandler<IEvent>>> _eventDictionary =
             new ConcurrentDictionary<string, List<IIntegrationEventHandler<IEvent>>>();
         private IIntegrationEventHandler<IEvent> _eventHandler;
-/*
-        public MemoEventBus(ConcurrentDictionary<string, List<IIntegrationEventHandler<IEvent>>> eventDictionary, IIntegrationEventHandler<IEvent> handler)
-        {
-            _eventDictionary = eventDictionary;
-            _handler = handler;
-        }
-*/
+
         public MemoEventBus()
         {
             _eventHandler = Activator.CreateInstance(typeof(IntegrationEventHandler)) as IIntegrationEventHandler<IEvent>;
@@ -22,7 +16,7 @@ namespace Eventer.Events
 
         public void Publish(IEvent @event)
         {
-            var eventTypeName = @event.EventTypeName;// typeof().FullName
+            var eventTypeName = @event.EventTypeName;
             if (_eventDictionary.ContainsKey(eventTypeName))
             {
                 foreach (var handler in _eventDictionary[eventTypeName])
@@ -32,9 +26,8 @@ namespace Eventer.Events
             }
         }
 
-        public void Subscribe(string eventTypeName) //, IIntegrationEventHandler<IEvent> eventHandler
+        public void Subscribe(string eventTypeName) 
         {
-           // var eventHandler = Activator.CreateInstance(typeof(IntegrationEventHandler)) as IIntegrationEventHandler<IEvent>;
             if (!_eventDictionary.ContainsKey(eventTypeName))
             {
                 _eventDictionary.TryAdd(eventTypeName, new List<IIntegrationEventHandler<IEvent>> { _eventHandler });
